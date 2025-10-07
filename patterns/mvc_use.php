61151b8ef4db9dc275e69471676f4b031e19b5b3
Andrew 'ImDoctor <130654327+xImDoctor@users.noreply.github.com>
2025-10-07 12:17:21 +0000

    Fix id error

--- patterns/mvc_use.php
+++ patterns/mvc_use.php
@@ -4,7 +4,7 @@ spl_autoload_register();
 use MVC\Controllers\Controller;
 
 //$obj = new Controller('users.rss');
-$obj = new Controller('users/1.html');
+$obj = new Controller('users/0.html');
 echo $obj->render();
 
 // под markdown
